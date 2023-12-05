dabc92de80a817cfc7b72abad1235218de9ef2b4
Serkan <s8ersis@gmail.com>
2023-12-05 07:40:46 +0300

    started not finished

---
+++ src/components/List.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import data from "../helper/data";
+const List = () => {
+  const [show, setShow] = useState(data);
+  const [number, setNumber] = useState(0);
+console.log(show.length);
+  const handleNextShow = () => {
+   if(number < show.length - 5){
+setNumber(number + 5)
+   }
+   else {
+   setNumber(0) 
+   }
+};
+console.log(number);
+
+
+  const handlePreviousShow = () => {
+   if(number >= 0){
+setNumber(number - 5)
+   }else {
+    setNumber(show.length - 5)
+   }
+};
+console.log(number); 
+
+//   const { id, image, name, age, email } = show;
+
+  return (
+    <>
+    {show.slice(number,number + 5).map(({ id, image, name, age, email }) =>
+      (<article className="person" key={id}>
+        <img src={image} alt={"name"} />
+        <div>
+          <h4>{name}</h4>
+          <p>{email}</p>
+          <p>{age} years</p>
+        </div>
+      </article>
+
+))}
+
+<div className="btns">
+        <button onClick={()=> handlePreviousShow()}>Prev</button>
+        <button onClick={() => handleNextShow()}>Next</button>
+      </div>
+    </>
+  );
+};
+
+export default List;
